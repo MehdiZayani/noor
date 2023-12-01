05e95546601d312dc0838a35bbd01d2dbde705b1
Badi's Macbook <macbook@Badis-Macbook.local>
2023-12-01 23:33:40 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Noor don't be sad</h1>
+      <h2>Your too gorgeous to be sad</h2>
+      <h3>This shit took me too long</h3>
+      <h4>Bisous</h4>
+    </main>
+  )
+}
