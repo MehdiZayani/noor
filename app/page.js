8c8bcdbec3facb3f54b7310436d2108da44fdbae
Badi's Macbook <macbook@Badis-Macbook.local>
2023-12-09 10:25:10 +0100

    projectfr

--- app/page.js
+++ app/page.js
@@ -3,10 +3,51 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Noor don't be sad</h1>
-      <h2>Your too gorgeous to be sad</h2>
-      <h3>This shit took me too long</h3>
-      <h4>Bisous</h4>
+      <div>
+       <h1 className='py-8 text-pink-500 text-lg'>L’amour (D'après moi):</h1>
+       <p>
+        Avant que les femmes ne l'utilisent pour manipuler, l'amour était un sentiment pur et sincère. Il était une force puissante qui pouvait unir les gens et créer des liens profonds. L'amour était un don, un cadeau que l'on offrait à l'autre sans rien attendre en retour.
+        Les femmes, en particulier, étaient considérées comme des êtres d'amour. Elles étaient vues comme les gardiennes de l'amour, les protectrices des cœurs brisés. Les femmes étaient censées être capables de donner et de recevoir l'amour sans condition.
+        Cependant, au fil du temps, l'amour a commencé à être utilisé pour manipuler. Les femmes ont commencé à utiliser l'amour pour obtenir ce qu'elles voulaient des hommes. Elles ont commencé à jouer avec les sentiments des hommes, à les faire tomber amoureux d'elles pour ensuite les contrôler.
+        Cette manipulation de l'amour a eu un impact négatif sur la société. Elle a contribué à créer une image négative des femmes, comme des êtres manipulateurs et sans cœur. Elle a également rendu plus difficile pour les hommes de faire confiance aux femmes.
+        Il est important de se rappeler que l'amour est un sentiment puissant, mais qu'il peut être utilisé à mauvais escient. Il est important de faire attention aux personnes qui utilisent l'amour pour manipuler.</p> 
+        <p>L’amour, une force puissante et parfois tumultueuse, a été le catalyseur de nombreuses intrigues historiques. Prenez Hélène de Troie, dont la beauté a déclenché une guerre légendaire. Son impact va bien au-delà d’une simple romance, soulignant comment l’amour peut être exploité pour des desseins politiques complexes. De même, Cleopâtre, reine égyptienne célèbre, a manœuvré avec adresse à travers des liaisons amoureuses pour protéger son royaume. Ces exemples illustrent que l’amour, lorsqu’il est utilisé habilement, peut devenir une arme subtile dans les jeux de pouvoir, où les femmes, tout comme les hommes, peuvent manœuvrer avec finesse pour atteindre leurs objectifs.</p>
+
+        <h2 className='py-8 text-pink-500'>Voici quelques exemples de manipulation de l'amour par les femmes :</h2>
+        <p>* Une femme peut faire croire à un homme qu'elle l'aime pour l'amener à faire quelque chose pour elle.</p>
+        <p>* Une femme peut utiliser l'amour pour faire sentir coupable un homme et le contrôler.</p>
+        <p>* Une femme peut utiliser l'amour pour détruire la confiance d'un homme.</p>
+
+        <p>Mais l’équilibre du pouvoir entre le dominé et le dominant prend une dimension particulière dans le contexte amoureux. Dans les relations intimes, il est fréquent de voir une oscillation entre ces rôles, chaque partenaire assumant tour à tour le contrôle et la soumission. Cela peut créer une danse passionnante de la dynamique émotionnelle, où la vulnérabilité et la confiance s’entremêlent. Lorsque le dominé peut être fort dans sa vulnérabilité et le dominant peut être doux dans sa force, une connexion profonde émerge. Cette fluidité des rôles permet une compréhension mutuelle plus profonde, où l’amour se nourrit de la capacité à s’adapter et à s’épanouir au fil du temps.</p>
+        <p>Bien sûr, les relations amoureuses ne sont pas toujours un terrain de jeux harmonieux. Parfois, la dynamique du dominé et du dominant peut basculer vers des extrêmes moins sains. Une domination excessive peut se transformer en contrôle manipulateur, tandis que le rôle du dominé peut conduire à une dépendance malsaine. Ces dynamiques toxiques peuvent étouffer la croissance individuelle et la relation elle-même. Il est crucial de reconnaître ces signes et de favoriser des discussions ouvertes pour prévenir les dérives néfastes, car l’amour sain nécessite avant tout le respect mutuel et l’autonomie.</p>
+        <h1 className='py-8 text-pink-500'>Je vous raconte mon histoire :</h1>
+
+        Une fois je suis tombé amoureux c’était soudain j’ai oublié comment mais je sais avec certitude que L’éclatante évidence de mon amour envers elle transcende les frontières du langage, s’exprimant à travers une symphonie d’émotions raffinées. Son charme exquis éveille en moi un émerveillement constant, chaque regard échangé étant une poésie silencieuse. Sa grâce, à la fois délicate et puissante, me captive, tissant un lien d’une profondeur incommensurable. Chaque trait de son être, empreint d’une beauté intérieure éblouissante, résonne en harmonie avec les battements de mon cœur épris. Cet amour, empreint d’une sincérité noble, devient une quête intemporelle, une aventure où chaque moment passé à ses côtés est un trésor inestimable, illuminant mon existence d’une lumière captivante.
+        je m’évanouir sur son beau sourire et je me réveille dans ses yeux 
+        suis-je entrain de rêver ?
+        qui est dont cette merveilleuse créature ?
+        r** ce nom est d'origine arabe. Dans cette langue, il signifie « vie, donner la vie ».
+        alors est t’elle un ange ? non elle est plus que ça…
+
+        elle me manques,donc je l’appelle.
+        ça voix, ça douce voix,  ça m’apaise,suis-je entrain de planer ? est-elle mon ecstasy? 
+        ma drogue du bonheur ? mon addiction ?
+
+        avec une douceur d’une plume je deviens fous, elle est entrain de me redéfinir la définition même de l’amour et de l’affection.
+        elle m’est sacrifiée, elle m’est chère 
+
+        <h1 className='py-8 text-pink-500'>Petit poème écrit par moi:</h1>
+
+        <p>Errant au sein d’une mélancolie, mon l'âme égarée se languit,</p>
+        <p>Tentant d'unir les brisures, entre expérience et l'inédit.</p>
+        <p>Évoluer ou demeurer, tel est mon éternel défi,</p> 
+        <p>L'essence flétrie, le cœur gémit, dans ce tumulte infini.</p>
+        <p>Suivre r** et s'épancher dans sa lumière</p>
+        <p>ou s'éloigner et souffrir.</p>
+        <p>Deux routes s'offrent, l'une effleure le bonheur et s'y pousse,</p>
+        <p>L'autre vers l'abîme, où la douleur s’invite.</p>
+        <p>mais dans les deux conjoncture mon cœur souffre.</p>
+      </div>
     </main>
   )
 }
